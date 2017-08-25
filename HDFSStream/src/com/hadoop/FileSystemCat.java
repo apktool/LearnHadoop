9120d19d079afed3eb5d03eaac06ffdea100da30
apktool <apktoolOo@gmail.com>
2017-08-25 10:33:53 +0800

    FileSystem cat

---
+++ HDFSStream/src/com/hadoop/FileSystemCat.java
@@ -0,0 +1,34 @@
+/**
+ * Description
+ * FileSystem Cat
+ *
+ * @author hadoop
+ * @version 0.1
+ * @since 8/25/17
+ */
+
+package com.hadoop;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+
+public class FileSystemCat {
+    public static void main(String[] args) throws IOException {
+        String uri = args[0];
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(URI.create(uri), conf);
+        InputStream in = null;
+        try {
+            in = fs.open(new Path(uri));
+            IOUtils.copyBytes(in, System.out, 4096, false);
+        } finally {
+            IOUtils.closeStream(in);
+        }
+    }
+}
