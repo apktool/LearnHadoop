99242a73f7805d0d59138a1eda984976721993a4
apktool <apktoolOo@gmail.com>
2017-08-25 09:58:00 +0800

    URLcat

---
+++ HDFSStream/README.md
@@ -0,0 +1,9 @@
+# Note
+
+本程序主要用于显示hdfs中指定文件的内容，因此需要在需要的地方放置好文件。
+
+输入参数根据需要设置如下
+
+```
+hdfs://localhost:9000/user/hadoop/input/sample.txt
+```
---
+++ HDFSStream/src/com/hadoop/URLcat.java
@@ -0,0 +1,30 @@
+
+package com.hadoop;
+
+import org.apache.hadoop.fs.FsUrlStreamHandlerFactory;
+import org.apache.hadoop.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+public class URLcat {
+
+    static {
+        URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
+    }
+
+    public static void main(String[] args) {
+        InputStream in = null;
+        try {
+            in = new URL(args[0]).openStream();
+            System.out.println(in);
+            IOUtils.copyBytes(in, System.out, 4096, false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            IOUtils.closeStream(in);
+        }
+
+    }
+}
