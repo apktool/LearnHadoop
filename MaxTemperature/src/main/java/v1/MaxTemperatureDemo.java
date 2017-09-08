1279a5b8351f8b0bba2a8f134675c89e0dc65140
apktool <apktoolOo@gmail.com>
2017-09-08 18:15:00 +0800

    MaxTemperature | v1 Basic MapReduce function

---
+++ MaxTemperature/src/main/java/v1/MaxTemperatureDemo.java
@@ -0,0 +1,46 @@
+/**
+ * Description
+ * Created with IntelliJ IDEA.
+ *
+ * @author apktool
+ * @version 0.1
+ * @since 9/8/17
+ */
+
+package v1;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class MaxTemperatureDemo {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        if (args.length != 2) {
+            System.err.println("Command error: command <input path> <output path>");
+            System.exit(-1);
+        }
+
+        Job job = Job.getInstance();
+        job.setJobName("Max Temperature");
+
+        job.setJarByClass(MaxTemperatureDemo.class);
+        job.setMapperClass(MaxTemperatureMapper.class);
+        job.setReducerClass(MaxTemperatureReducer.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
---
+++ MaxTemperature/src/main/java/v1/MaxTemperatureMapper.java
@@ -0,0 +1,28 @@
+/**
+ * Description
+ * Created with IntelliJ IDEA.
+ *
+ * @author apktool
+ * @version 0.1
+ * @since 9/8/17
+ */
+
+package v1;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class MaxTemperatureMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        String year = line.substring(15, 19);
+
+        int airTemperature = Integer.parseInt(line.substring(87, 92));
+        context.write(new Text(year), new IntWritable(airTemperature));
+    }
+}
