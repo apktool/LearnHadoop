e448e534e8befc075ae0802576f7b9ab5bca56f9
apktool <apktoolOo@gmail.com>
2017-09-12 16:10:15 +0800

    Kafka Prodecure and Consumer Demo

---
+++ SendRecordDemo/src/main/java/ConsumerDemo.java
@@ -0,0 +1,35 @@
+/**
+ * Description
+ * Consume given topic 
+ *
+ * @author apktool
+ * @version 0.1
+ * @since 9/12/17
+ */
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerDemo {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "test");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+        consumer.subscribe(Arrays.asList("foo", "bar", "my-topic"));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+            }
+        }
+    }
+}
---
+++ SendRecordDemo/src/main/java/ProducerDemo.java
@@ -0,0 +1,35 @@
+/**
+ * Description
+ * Produce new record related to given topic
+ *
+ * @author apktool
+ * @version 0.1
+ * @since 9/11/17
+ */
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class ProducerDemo {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        producer.send(new ProducerRecord<String, String>("my-topic", "热血", "火影忍者"));
+        producer.send(new ProducerRecord<String, String>("my-topic", "日常", "今天要来点兔子吗?"));
+
+        producer.close();
+    }
+}
